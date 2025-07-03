bd21658d3573c09292c7458793bbfe6fd1fa004c
dandino4real <ifyd47@gmail.com>
2025-07-03 01:10:26 +0100

    updated vercel.json

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import app from './server';
 
 
 // const env = process.env.NODE_ENV || 'development';
-// const PORT = process.env['APP_PORT'] || 3001; 
+const PORT = process.env['APP_PORT'] || 3001; 
 
 
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running at http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
 
-export default app;
+// export default app;
