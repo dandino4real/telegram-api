637d803178e5bfc567cb1c22968899646aa6972d
dandino4real <ifyd47@gmail.com>
2025-07-03 18:30:34 +0100

    update config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  const mongoUri = process.env.MONGO_URI;
+  const mongoUri = process.env.MONGODB_URI;
   
   if (!mongoUri) {
     throw new Error("MONGO_URI is not defined in environment variables");
