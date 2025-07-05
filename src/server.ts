d9d468106bc18eeb2a86b370072bb3956583e7db
dandino4real <ifyd47@gmail.com>
2025-07-05 15:45:39 +0100

    updated domain

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,7 @@ app.use(cookieParser());
 
 const corsOrigins = process.env.CORS_ORIGINS
   ? process.env.CORS_ORIGINS.split(",").map((origin) => origin.trim())
-  : ["https://afibie-fx.vercel.app"];
+  : [".vercel.app"];
 
 app.use(
   cors({
